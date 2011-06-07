54dec7f4c5427ef3b028b4c6a8d6cf9fbb4b493d
Artem Kotsiuba <artemkotsiuba@gmail.com>
2011-06-07 23:47:57 +0300

    English comments

---
+++ Game.cpp
@@ -0,0 +1,26 @@
+#include "Game.h"
+
+void CGame::AddStep(CStepMsg *i_step)
+{
+
+}
+
+void CGame::GetState()
+{
+    m_lock
+}
+
+void CGame::Start()
+{
+
+}
+
+void CGame::run()
+{
+
+}
+
+void CGame::recalculation()
+{
+
+}
